1bec86c8577e40b577262baf5882b3e50aa469f8
Greenhair <aukeswen2001@yahoo.com>
2021-12-04 18:22:03 +0100

    update query for rechnung

--- src/rechnung/dto/RechnungQuery.ts
+++ src/rechnung/dto/RechnungQuery.ts
@@ -1,15 +1,19 @@
+import { Type } from "class-transformer";
 import { IsInt, IsOptional } from "class-validator";
 
 export class RechnungQuery {
     @IsOptional()
+    @Type(() => Number)
     @IsInt()
     year: number
-
+    
     @IsOptional()
+    @Type(() => Number)
     @IsInt()
     month: number
     
     @IsOptional()
+    @Type(() => Number)
     @IsInt()
     week: number
 }
